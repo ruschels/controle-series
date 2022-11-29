fe709042d1880a36ccf0f890e916730845056858
ruschels <ruschels>
2022-11-29 10:35:54 -0300

    Refiz os forms sem o component de form

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,4 +1,16 @@
 <x-layout title="Nova Série">
-	<x-series.form :action="route('series.store')" :nome="old('nome')" :update="false" /> <!-- definimos a variavel $action que é preenchida no action do form.blade -->
 
+	<form action="{{ route('series.store') }}" method="post">
+		@csrf <!-- diretiva do blade que evita ataque de XSS e CSRF -->
+				<div class="mb-3">
+				<label class="form-label" for="nome">Nome:</label>
+				<input class="form-control" 
+						type="text" 
+						id="nome" 
+						name="nome"	
+						value="{{ old('nome') }}"				
+						>
+			</div>
+			<button type="submit" class="btn btn-primary mt-2">Adicionar</button>
+		</form>
 </x-layout>
--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,4 +1,17 @@
-<x-layout title="Editar Série '{{$serie->nome}}'">
-	<x-series.form :action="route('series.update', $serie->id)" :nome="$serie->nome" :update="true"/> <!-- enviamos também a variavel $nome para ser preenchida no form -->
+<x-layout title="Editar Série '{!! $serie->nome !!}'">
 
+		<form action="{{ route('series.update', $serie->id) }}" method="post">
+		@csrf <!-- diretiva do blade que evita ataque de XSS e CSRF -->
+		@method("PUT");
+			<div class="mb-3">
+				<label class="form-label" for="nome">Nome:</label>
+				<input class="form-control" 
+						type="text" 
+						id="nome" 
+						name="nome"	
+						value="{{ $serie->nome }}"				
+						>
+			</div>
+			<button type="submit" class="btn btn-primary mt-2">Adicionar</button>
+		</form>
 </x-layout>
