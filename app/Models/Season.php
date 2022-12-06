ba1d5f4274309f1a85e44f5e8d9761a536ae00c4
ruschels <ruschels>
2022-12-05 22:29:03 -0300

    mostrando os episódios assistidos também na view da temporada

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Episode;
 
 class Season extends Model
 {
@@ -18,5 +19,11 @@ class Season extends Model
     public function episodes()
     {
         return $this->hasMany(Episode::class);
+    }
+
+    public function numberOfWatchedEpisodes() :int {
+        return $this->episodes
+            ->filter(fn ($episode) => $episode->watched)
+            ->count();
     }
 }
