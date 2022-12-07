1e6c9d0713bddbf87f31956ed763007910a83fc3
ruschels <ruschels>
2022-12-06 22:59:08 -0300

    elementos diferentes para logados(auth) e deslogados(guest)

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,12 +1,12 @@
-<x-layout title="login">
+<x-layout title="Controle de Séries - Login">
 	<form method="post">
         @csrf
-        <div class="form-group text-light">
+        <div class="form-group">
             <label for="email" class="form-label">E-mail</label>
             <input type= "email" name="email" id="email" class="form-control">        
         </div> 
 
-        <div class="form-group text-light">
+        <div class="form-group">
             <label for="password" class="form-label">Password</label>
             <input type= "password" name="password" id="password" class="form-control">        
         </div> 
